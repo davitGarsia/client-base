24cf9b77bc6fc6f7538c90546c7df7f1b8508448
davit.garsiashvili <daviti.garsiashvili@ugt.ge>
2025-02-11 23:19:03 +0400

    fix: phone-number validator

--- src/app/shared/validators/phone-number-validator.ts
+++ src/app/shared/validators/phone-number-validator.ts
@@ -3,9 +3,12 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export function startsWithFiveValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     const value = control.value;
-    console.log('value:', typeof value);
 
-    if (typeof value === 'string' && value !== 'null' && value !== 'NaN' && value.startsWith('5')) {
+    if (!value) {
+      return null;
+    }
+
+    if (typeof value === 'string' && value.trim().startsWith('5')) {
       return null;
     }
 
